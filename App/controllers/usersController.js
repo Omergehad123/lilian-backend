6422785f10f4ea400727215ee3495294cde27d87
omergehad405 <omergehad593@gmail.com5>
2026-01-21 02:04:05 +0200

    update

--- App/controllers/usersController.js
+++ App/controllers/usersController.js
@@ -223,7 +223,7 @@ const loginAsGuest = asyncWrapper(async (req, res, next) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 1000 * 60 * 60 * 24 * 7,
   });
 
